950f1b426dff014c30fb7b04eb643428233f54a6
Jake <87618544+Jxk0be@users.noreply.github.com>
2023-09-28 08:00:59 -0400

    feat: Setup skeleton of our app (#1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Home Page</div>,
+    errorElement: <h1 className="text-4xl font-bold text-red-500">404 Error</h1>
+  },
+  {
+    path: "/contact",
+    element: <h1>contact</h1>
+  }
+]);
+
 function App() {
 
   return (
     <>
-      <h1 className="bg-black">Add</h1>
+      <RouterProvider router={router} />
     </>
   )
 }
